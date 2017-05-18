8a0e05fbf6e7f1bc1d74ed5fa3241b060756a269
Derek Michael Frank <derekmfrank@gmail.com>
2017-05-18 08:56:21 -0700

    refactor(bot): Convert to object-oriented and remove @mention requirement
    The bot will respond to any channel it is listening to when a user sends
    a non-empty message.

--- slackbot.py
+++ slackbot.py
@@ -14,59 +14,72 @@ from commands.weather import get_weather
 
 logger = logging.getLogger(__name__)
 
-# constants
-AT_BOT = "<@" + os.environ.get("BOT_ID") + ">"
-EXAMPLE_COMMAND = "do"
+# Constants.
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_ID = os.environ.get('BOT_ID')
+AT_BOT = '<@{id}>'.format(id=BOT_ID)
+NEWS_CHANNEL = 'news'
 
-# instantiate Slack & Twilio clients
-slack_client = SlackClient(os.environ.get('BOT_TOKEN'))
-
-cmd_names = ('mood', 'celebration', 'num_posts', '100day_tweet', 'weather')
-cmd_functions = (get_mood, celebration, get_num_posts, create_tweet, get_weather)
+cmd_names = ['mood', 'celebration', 'num_posts', '100day_tweet', 'weather']
+cmd_functions = [get_mood, celebration, get_num_posts, create_tweet, get_weather]
 COMMANDS = dict(zip(cmd_names, cmd_functions))
 
 
-def parse_slack_rtm_events(events):
-    """
+class NewsSlackBot(object):
+
+    def __init__(self, read_websocket_delay=1):
+        self.read_websocket_delay = read_websocket_delay
+
+        # instantiate Slack & Twilio clients
+        self.slack_client = SlackClient(BOT_TOKEN)
+
+    def loop_forever(self):
+        if self.slack_client.rtm_connect():
+            logger.info('StarterBot connected and running!')
+            while True:
+                command, channel = self.parse_rtm_events(self.slack_client.rtm_read())
+                if command and channel:
+                    self.handle_command(command, channel)
+                sleep(self.read_websocket_delay)
+        else:
+            logger.critical('Connection failed. Invalid Slack token or bot ID?')
+
+    @staticmethod
+    def parse_rtm_events(events):
+        """
         The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
-    logger.debug(events)
-    for evt in events:
-        text = evt.get('text', '')
-        if AT_BOT in text:
-            # return text of an @mention, whitespace and @mention removed
-            return ' '.join(t.strip().lower() for t in text.split(AT_BOT) if t.strip()), \
-                   evt['channel']
-    return None, None
+        This parsing function returns any for any non-empty message.
 
+        """
+        logger.debug(events)
+        for evt in events:
+            text = evt.get('text')
+            if evt.get('user') == BOT_ID or not text:
+                continue
+            elif evt['type'] == 'message':
+                # return text, whitespace and @mention removed
+                tokens = filter(None, (t.strip() for t in text.split(AT_BOT)))
+                return ' '.join(t.lower() for t in tokens), evt['channel']
+        return None, None
 
-def handle_command(cmd, channel):
-    
-    cmd = cmd.split()
-    cmd, args = cmd[0], cmd[1:]
+    def handle_command(self, cmd, channel):
+        cmd = cmd.split()
+        cmd, args = cmd[0], cmd[1:]
 
-    if cmd in COMMANDS:
-        response = COMMANDS[cmd](*args)
-    else:
-        response = ('Not sure what you mean? '
-		    'I can help you with these commands:\n'
-		    '{}'.format('\n'.join(cmd_names)))
+        if cmd in COMMANDS:
+            response = COMMANDS[cmd](*args)
+        else:
+            response = ('Not sure what you mean? '
+                        'I can help you with these commands:\n'
+                        '{}'.format('\n'.join(cmd_names)))
 
-    slack_client.api_call('chat.postMessage', channel=channel,
-                          text=response, as_user=True)
+        self.slack_client.api_call('chat.postMessage',
+                                   channel=channel,
+                                   text=response,
+                                   as_user=True)
 
 
 if __name__ == '__main__':
     logging.basicConfig(filename='bot.log', level=logging.DEBUG)
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-    if slack_client.rtm_connect():
-        logger.info('StarterBot connected and running!')
-        while True:
-            command, channel = parse_slack_rtm_events(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            sleep(READ_WEBSOCKET_DELAY)
-    else:
-        logger.critical('Connection failed. Invalid Slack token or bot ID?')
+    slackbot = NewsSlackBot()
+    slackbot.loop_forever()
