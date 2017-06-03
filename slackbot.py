3e75bc52d978fc3f4708c663b2da647ce90a3751
Derek Michael Frank <derekmfrank@gmail.com>
2017-06-03 08:21:27 -0700

    style(slackbot): Fix for linter

--- slackbot.py
+++ slackbot.py
@@ -1,11 +1,9 @@
 import logging
-import os
-import sys
-import yaml
 from datetime import datetime, timedelta
 from operator import itemgetter
 from time import sleep
 
+import yaml
 from slackclient import SlackClient
 
 if __name__ == '__main__':
@@ -15,36 +13,36 @@ if __name__ == '__main__':
 from newsclient.twitter import TwitterNews
 
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 class NewsSlackBot(object):
 
-    def __init__(self, secrets_yaml='secrets.yml', loop_delay=1, news_interval=60):
-        self.loop_delay = loop_delay  # In seconds.
-        self.interval = timedelta(minutes=news_interval)
-        self.last_news_update = datetime.now() - timedelta(minutes=2*news_interval)
-
-        with open(secrets_yaml, 'rb') as fp:
-            self.settings = yaml.load(fp)
-        self.bot_token = self.settings['slackbot']['token']
-        self.bot_name = self.settings['slackbot'].get('name')
-        self.bot_id = self.settings['slackbot'].get('id')
+    def __init__(self, secrets_yaml='secrets.yml'):
+        with open(secrets_yaml, 'rb') as secrets_file:
+            self.config = config = yaml.load(secrets_file)
+        self.bot = bot = {
+            'token': config['slackbot']['token'],
+            'id': config['slackbot'].get('id'),
+            'name': config['slackbot'].get('name'),
+        }
 
         # Instantiate Slack client.
-        self.slack_client = sc = SlackClient(self.bot_token)
+        self.slack_client = SlackClient(bot['token'])
 
         # Set bot id.
-        if not self.bot_id:
-            if not self.bot_name:
+        if not bot['id']:
+            if not bot['name']:
                 raise EnvironmentError
 
             users_list = self.get('users.list')
             if not users_list.get('ok'):
                 raise EnvironmentError
             users = [u for u in users_list.get('members') if 'name' in u]
-            self.bot_id = next(u.get('id') for u in users if u['name'] == self.bot_name)
-        self.at_bot = '<@{id}>'.format(id=self.bot_id)
+            bot['id'] = next(u.get('id') for u in users if u['name'] == bot['name'])
+        bot['at'] = '<@{id}>'.format(id=bot['id'])
+
+        self._channels = None, None
 
     def get(self, name, key=None):
         result = self.slack_client.api_call(name)
@@ -52,35 +50,36 @@ class NewsSlackBot(object):
 
     @property
     def channels(self):
-        last_update, channels = getattr(self, '_channels', (None, None))
+        last_update, channels = self._channels
 
         now = datetime.now()
         if last_update is None or now - last_update > timedelta(minutes=10):
             last_update = now
             self._channels = last_update, channels = now, [
-                    c
-                    for c in self.get('channels.list', 'channels') if c['is_member']]
+                c
+                for c in self.get('channels.list', 'channels') if c['is_member']]
 
         return channels
 
     def loop_forever(self):
         if self.slack_client.rtm_connect():
-            logger.info('StarterBot connected and running!')
+            LOGGER.info('StarterBot connected and running!')
+            interval = timedelta(minutes=60)
+            last_news_update = datetime.now() - 2*interval
             while True:
                 now = datetime.now()
-
                 command, channel = self.parse_rtm_events(self.slack_client.rtm_read())
                 if command and channel:
                     # Respond to messages.
                     self.handle_command(command, channel)
-                elif now - self.last_news_update > self.interval:
+                elif now - last_news_update > interval:
                     # Send news updates.
-                    self.last_news_update = now
+                    last_news_update = now
                     self.send_news()
 
-                sleep(self.loop_delay)
+                sleep(1)
         else:
-            logger.critical('Connection failed. Invalid Slack token or bot ID?')
+            LOGGER.critical('Connection failed. Invalid Slack token or bot ID?')
 
     def parse_rtm_events(self, events):
         """
@@ -88,37 +87,36 @@ class NewsSlackBot(object):
         This parsing function returns any for any non-empty message.
 
         """
+        at_bot = self.bot['at']
         for evt in events:
-            logger.debug(evt)
+            LOGGER.debug(evt)
             channel, text = evt.get('channel'), evt.get('text')
-            if evt.get('user') == self.bot_id or not text or evt['type'] != 'message':
+            if evt.get('user') == self.bot['id'] or not text or evt['type'] != 'message':
                 continue
-            elif self.at_bot in text \
+            elif at_bot in text \
                     or channel not in map(itemgetter('id'), self.channels):
                 # Handle command only if @mention or direct channel (IM).
-                tokens = filter(None, (t.strip() for t in text.split(self.at_bot)))
+                tokens = filter(None, (t.strip() for t in text.split(at_bot)))
                 # return text, whitespace and @mention removed
                 return ' '.join(t.lower() for t in tokens), channel
         return None, None
 
     def handle_command(self, cmd, channel):
-        cmd = cmd.split()
-        cmd, args = cmd[0], cmd[1:]
-
+        cmd = cmd.split()[0]
         response = 'I am unable to process commands at this time...'
-
         self.slack_client.api_call('chat.postMessage',
                                    channel=channel,
                                    text=response,
                                    as_user=True)
 
     def get_newsclients(self):
-        return [TwitterNews(self.settings['twitter'])]
+        return [TwitterNews(self.config['twitter'])]
 
     def send_news(self):
         for client in self.get_newsclients():
             for channel in self.channels:
-                for article in client.fetch(topic=channel['topic']['value'] or channel['purpose']['value']):
+                for article in client.fetch(topic=channel['topic']['value']
+                                            or channel['purpose']['value']):
                     self.slack_client.api_call('chat.postMessage',
                                                channel=channel['id'],
                                                text=article,
